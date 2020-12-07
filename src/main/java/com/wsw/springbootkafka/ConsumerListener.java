88d331d755e8bd6664f61050007518142199ef7c
wangsongwen <2544894086wsw@gmail.com>
2020-12-07 16:15:57 +0800

    2020-12-7 update

--- src/main/java/com/wsw/springbootkafka/ConsumerListener.java
+++ src/main/java/com/wsw/springbootkafka/ConsumerListener.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ConsumerListener {
-    @KafkaListener(topics = "testTopic")
+    @KafkaListener(id = "wswGroup", topics = "testTopic")
     public void onMessage(String message) {
         System.out.println(message);
     }
